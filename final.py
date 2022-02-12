008752ed72a32e77507848f63584ce59869a68b2
tabithavimal21 <95914849+tabithavimal21@users.noreply.github.com>
2022-02-12 07:56:34 +0530

    Add files via upload

---
+++ final.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import seaborn as sns
+
+df = pd.read_csv("final.csv")
+
+X = []
+
+mass = df["Mass"].to_list()
+radius = df["Radius"].to_list()
+dist = df["Distance"].to_list()
+gravity = df["Gravity"].to_list()
+
+mass.sort()
+radius.sort()
+gravity.sort()
+plt.plot(radius,mass)
+
+plt.title("Radius & Mass of the Star")
+plt.xlabel("Radius")
+plt.ylabel("Mass")
+plt.show()
+
+plt.plot(mass,gravity)
+
+plt.title("Mass vs Gravity")
+plt.xlabel("Mass")
+plt.ylabel("Gravity")
+plt.show()
+
+plt.scatter(radius,mass)
+plt.xlabel("Radius")
+plt.ylabel("Mass")
+plt.show()
+
